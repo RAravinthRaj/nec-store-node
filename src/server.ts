5f9cb844dfeb788c7f61081bc9905bc9821052ad
Aravinth Raj R <137486197+RAravinthRaj@users.noreply.github.com>
2025-10-22 15:20:46 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-//app.use(corsMiddleware);
+app.use(corsMiddleware);
 app.use(rate_limiter);
 app.use(bodySizeLimit);
 app.use(httpsRedirect);
